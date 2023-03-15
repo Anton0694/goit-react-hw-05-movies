c4ee87eaeed7a87a169fd08c91fbe2277207f6bb
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-03-15 18:05:15 +0200

    +

--- src/API.js
+++ src/API.js
@@ -11,7 +11,7 @@ export const getTrandingMovies = () => {
 return axios.get(`${baseURL}/3/trending/all/day?api_key=${API_KEY}`)
 }
 
-export const getAllMovies = () => {
-return axios.get(`${baseURL}/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`)
+export const getMovieName = (query) => {
+return axios.get(`${baseURL}/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`)
 }
 
