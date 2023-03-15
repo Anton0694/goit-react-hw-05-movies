069ef2102d280f2a15f2ca9dd20c8a7c84caa0ab
Anton0694 <91780319+Anton0694@users.noreply.github.com>
2023-03-15 19:27:25 +0200

    +

--- src/API.js
+++ src/API.js
@@ -1,17 +1,35 @@
 import axios from 'axios';
 
 const API_KEY = '0c6df8fcb0cf1f0f5728a6c0564c7c5a'
-const baseURL = 'https://api.themoviedb.org'
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
-export const getMovieById = (id) => {
-    return axios.get(`${baseURL}/3/movie/${id}?api_key=${API_KEY}`)
-}
+export const getMovieById = async id => {
+    try {
+        const response = await axios.get(
+            `/movie/${id}?api_key=${API_KEY}`);
+        return response;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 
-export const getTrandingMovies = () => {
-return axios.get(`${baseURL}/3/trending/all/day?api_key=${API_KEY}`)
-}
+export const getTrandingMovies = async () => {
+     try {
+        const response = await axios.get(
+            `/trending/all/day?api_key=${API_KEY}`);
+        return response;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 
-export const getMovieName = (query) => {
-return axios.get(`${baseURL}/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`)
-}
+export const getMovieName = async query => {
+    try {
+        const response = await axios.get(
+            `/search/movie?api_key=${API_KEY}&page=1&query=${query}`);
+        return response;
+    } catch (error) {
+        console.log(error.message);
+    }
+};
 
